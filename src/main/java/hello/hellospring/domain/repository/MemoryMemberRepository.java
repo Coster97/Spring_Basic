ebb8fdf60f8e30f908877c76fe132bd885701893
박은경 <dmsrud1501222@naver.com>
2023-03-23 14:48:10 +0900

    20230323

---
+++ src/main/java/hello/hellospring/domain/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package hello.hellospring.domain.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; //키값을 생성해줄거임 윗줄 코드에서도 언급하신 동시성 문제?? 뭐지
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //근데 만약 결과가 null 이라면? 그때를 대비해 옵셔널을 사용.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //하나라도 찾아내라. 끝까지 없으면 옵셔널에 Null 포함되어 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+
+    public void clearStore(){
+        store.clear();
+    }
+}
